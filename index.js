547b40c693bbe29a6c40b28f6e6511a70720673a
Minhaz Jisun <minhaz.jisun@gmail.com>
2024-06-26 12:56:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+app.get('/', (req, res) => {
+  res.status(200).send(`Hello, from Node App on PORT: ${port}!`);
+});
+
+
+app.listen(port, () => {
+  console.log(`App running on port :${port}`);
+});
